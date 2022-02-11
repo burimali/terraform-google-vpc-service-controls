21facf120fcc5e01f0615d77e8e2db6076dfc6f2
burimali <65596300+burimali@users.noreply.github.com>
2022-02-11 15:49:27 +0100

    Update main.tf

--- examples/simple_example_with_ingress_rule/main.tf
+++ examples/simple_example_with_ingress_rule/main.tf
@@ -1,32 +1,20 @@
-/**
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-module "access_context_manager_policy" {
-  source      = "../.."
-  parent_id   = var.parent_id
-  policy_name = var.policy_name
-}
-
+#module "access_context_manager_policy" {
+#  source      = "terraform-google-modules/vpc-service-controls/google//.."
+#  parent_id   = var.parent_id
+#  policy_name = var.policy_name
+# }  
+  
 module "access_level_members" {
-  source      = "../../modules/access_level"
+  source      = "terraform-google-modules/vpc-service-controls/google//modules/access_level"
   description = "Simple Example Access Level"
-  policy      = module.access_context_manager_policy.policy_id
-  name        = var.access_level_name
-  members     = var.members
-  regions     = var.regions
+  policy = var.policy_id
+  name = "testing"
+#  policy      = module.access_context_manager_policy.policy_id
+#  name        = var.access_level_name
+#  name = "testing"
+#  members     = var.members
+  ip_subnetworks = ["8.8.8.8"]
+#  regions     = var.regions
 }
 
 resource "null_resource" "wait_for_members" {
@@ -38,23 +26,58 @@ resource "null_resource" "wait_for_members" {
 }
 
 module "regular_service_perimeter_1" {
-  source         = "../../modules/regular_service_perimeter"
-  policy         = module.access_context_manager_policy.policy_id
+  source         = "terraform-google-modules/vpc-service-controls/google//modules/regular_service_perimeter"
+  #policy         = module.access_context_manager_policy.policy_id
+  policy         = var.policy_id
   perimeter_name = var.perimeter_name
 
   description   = "Perimeter shielding bigquery project"
   resources     = [var.protected_project_ids["number"]]
   access_levels = [module.access_level_members.name]
 
-  restricted_services = ["bigquery.googleapis.com", "storage.googleapis.com"]
+  restricted_services = ["bigquery.googleapis.com", "storage.googleapis.com","compute.googleapis.com"]
+
 
   ingress_policies = [
-    {
+            {
+               from = {
+                   identities    = []
+                   identity_type = "ANY_IDENTITY"
+                   sources = {
+                      access_level = "*"
+                    }
+                }
+               to = {
+                   resources = ["*"]
+                   operations = {
+                   "*" = {
+                   methods = []
+                   }
+              }
+          }
+       }
+    ]
+  egress_policies = [ 
+      {
+              from = {
+                  identities    = []
+                  identity_type = "ANY_IDENTITY"
+                }
+               to = {
+                   resources = ["*"]
+                   operations = {
+                   "*" = {
+                   methods = []
+                   }
+              }
+           }
+        },
+       {
       "from" = {
         "sources" = {
           access_levels = ["*"] # Allow Access from everywhere
         },
-        "identities" = var.read_bucket_identities
+        "identities" = []
       }
       "to" = {
         "resources" = [
@@ -69,25 +92,134 @@ module "regular_service_perimeter_1" {
           }
         }
       }
-    },
-  ]
+    }
+        #  {
+        #       "from" = {
+        #           "identities"    = []
+        #           "identity_type" = "ANY_IDENTITY"
+        #         }
+        #        "to" = {
+        #            "resources" = [                      
+        #                "*"
+        #            ]
+        #            "operations" = {
+        #            "compute.googleapus.com" = {
+        #            "methods" = [
+        #                      "NetworkServices.list"
+        #                      ]
+        #                 }
+        #            }
+        #       }
+        #   }
+     ]
+ }
+#identity_type  = "ANY_IDENTITY"
+#    identities     = null
+#    sources        = [{
+#      resource     = ["*"]
+#      access level = null
+#    }]
+#    resource       = ["*"]
+#    operations     = null
+#},
+#]
 
-  shared_resources = {
-    all = [var.protected_project_ids["number"]]
-  }
+#    {
+#      "from" = {
+#        "sources" = {
+#          access_levels = ["*"] # Allow Access from everywhere
+#        },
+#        "identities" = var.read_bucket_identities
+#      }
+#      "to" = {
+#        "resources" = [ "*" ]
+#        "operations" = {
+#          "storage.googleapis.com" = {
+#            "methods" = [
+#              "google.storage.objects.get",
+#              "google.storage.objects.list"
+#            ]
+#          }
+#        }
+#      }
+#    },
 
-  depends_on = [
-    module.gcs_buckets
-  ]
-}
+#  {
+#    "from" = {
+#        "sources" = {
+#          access_levels = ["*"] # Allow Access from everywhere
+#        },
+#        "identities" = null
+#      }
+#
+#
+  #    "to" = {
+  #      "resources" = [ "*" ]
+  #      "operations" = null
+  #    }
+  #  },  
+  #]
 
 
-module "gcs_buckets" {
-  source           = "terraform-google-modules/cloud-storage/google"
-  project_id       = var.protected_project_ids["id"]
-  names            = var.buckets_names
-  randomize_suffix = true
-  prefix           = var.buckets_prefix
-  set_admin_roles  = true
-  admins           = var.members
-}
+
+#ingress_policies_02 = [
+#    {
+#      "from" = {
+#        "sources" = {
+#          access_levels = ["*"] # Allow Access from everywhere
+#        },
+#        "identities" = null
+#      }
+#      "to" = {
+#        "resources" = [ "*" ]
+#        "operations" = [ "*" ]
+#      }
+#    },
+#  ]
+
+    
+#    ingress_policies_02 = [
+#    {
+#      "from" = {
+#        "sources" = {
+#          access_levels = ["*"] # Allow Access from everywhere
+#        },
+#        "identities" = var.read_bucket_identities
+#      }
+#      "to" = {
+#        "resources" = [
+#          "*"
+#        ]
+#        "operations" = {
+#          "storage.googleapis.com" = {
+#            "methods" = [
+#              "google.storage.objects.get",
+#              "google.storage.objects.list"
+#            ]
+#          }
+#        }
+#      }
+#    },
+#]
+    
+#    
+#  shared_resources = {
+#    all = [var.protected_project_ids["number"]]
+#  }
+#
+#  depends_on = [
+#    module.gcs_buckets
+#  ]
+#}
+
+
+#module "gcs_buckets" {
+#  source           = "terraform-google-modules/cloud-storage/google"
+#  project_id       = var.protected_project_ids["id"]
+#  names            = var.buckets_names
+#  randomize_suffix = true
+#  prefix           = var.buckets_prefix
+#  set_admin_roles  = true
+#  admins           = var.members
+#}
+  
